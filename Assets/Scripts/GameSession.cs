e9167149423408ef40d65daec76bfd1ed99c9490
swiackichris <>
2018-09-17 21:50:10 +0200

    Arcanoid 1.00

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour {
+
+    // config params
+    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 10;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        scoreText.text = currentScore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Time.timeScale = gameSpeed;
+    // You can gradually increase the gameSpeed as the time passes.
+	}
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 32.0f;
+    [SerializeField] float minXClamp = 2.0f;
+    [SerializeField] float maxXClamp = 16.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits * 2;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minXClamp, maxXClamp);
+        transform.position = paddlePos;
+	}
+}
